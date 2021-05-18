6d8761cb22ae5dc20807cb9130999c53e472d727
Lucas-Pipo <lucasvieira91@hotmail.com>
2021-05-18 12:32:37 -0300

    Minhas aulas já salvas
    Primeiro push pra ver se tá tudo ok

---
+++ Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}Km/h!`)
+if (vel > 60){ //condição simples! Já que não tm else.
+    console.log("Você ultrapassouu a velocidade permitida. MULTADO!")
+}
+console.log('Dirija sempre usando cinto de segurança!')
